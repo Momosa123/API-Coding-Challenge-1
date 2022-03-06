c42f54bb1ea9a058185df56599fe548cdc65c8ed
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-03-06 21:05:19 +0100

    rendering country details into HTML

--- script.js
+++ script.js
@@ -12,6 +12,31 @@
 // }
 
 // whereAmI(48.824, 2.272);
+const container = document.getElementById("container");
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response) => {
+      if (!response.ok)
+        throw new Error(`Country not found (${response.status})`);
+
+      return response.json();
+    })
+    .then((data) => {
+      // const population = +data[0].capital;
+      // console.log((population / 100000).toFixed(2));
+      container.insertAdjacentHTML(
+        "beforeend",
+        `<img class="flag-img"src="${data[0].flags.png}">
+        <p>The country is called ${data[0].altSpellings[1]} </p>
+       <div><p>The capital is ${data[0].capital} </p>
+       <p>There are ${(+data[0].population / 1000000).toFixed(
+         2
+       )} Million people</p>
+       </div> `
+      );
+    });
+};
+// console.log(data[0].flags.png)
 
 function whereAmI(lat, long) {
   fetch(
@@ -26,10 +51,8 @@ function whereAmI(lat, long) {
     .then((result) => {
       return result.features[0].properties.country;
     })
+    .then((country) => getCountryData(country))
     .catch((err) => console.log(`${err} 💥💥💥`));
 }
 
 whereAmI(48.824, 2.272);
-const getCountryData = function (country) {
-  fetch(`https://restcountries.eu/rest/v2/name/${country}`);
-};
