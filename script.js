aa4241d17828e4dae9a494bff946dea7e66a76cd
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-03-06 19:07:44 +0100

    adding the render country API

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+// function whereAmI(lat, long) {
+//   fetch(`https://geocode.xyz/${lat},${long}?geoit=json`)
+//     .then((response) => {
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+//       response.json();
+//     })
+//     .then((data) => console.log(`I live in ${data.country}`))
+//     .catch((err) => {
+//       console.error(`${err} 💥💥💥`);
+//     });
+// }
+
+// whereAmI(48.824, 2.272);
+
 function whereAmI(lat, long) {
   fetch(
-    `https://geocode.xyz/${lat},${long}?geoit=json&auth=217766118136315206757x13894 `
+    `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long}&apiKey=7e427ba6bf7d4175b5323d39887653cd`
   )
-    .then((jsonLocation) => jsonLocation.json())
-    .then((data) => console.log(data));
+    .then((response) => {
+      if (!response.ok)
+        throw new Error(`Country not found (${response.status})`);
+
+      return response.json();
+    })
+    .then((result) => {
+      return result.features[0].properties.country;
+    })
+    .catch((err) => console.log(`${err} 💥💥💥`));
 }
 
 whereAmI(48.824, 2.272);
+const getCountryData = function (country) {
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`);
+};
